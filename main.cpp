0dbfe36ae8d9e7c2a8a42533db7ae27a28e685a2
stranger1sag <2295505925@qq.com>
2026-04-13 21:43:37 +0800

    create a window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,76 @@
-#include <iostream>
-#include <glad/glad.h>
 #include <SDL2/SDL.h>
-int main(int argc, char* argv[]){
-	std::cout<<"Hello World!"<<std::endl;
-	return 0;
+#include <glad/glad.h>
+#include <stdio.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+int main(int argc, char* argv[]) {
+    // 初始化 SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // 设置 OpenGL 版本 (3.3 Core)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // 创建窗口
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2 + OpenGL",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // 创建 OpenGL 上下文
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        printf("SDL_GL_CreateContext Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // 初始化 GLAD
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        printf("GLAD 初始化失败!\n");
+        return 1;
+    }
+
+	// 设置版本
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // 输出 OpenGL 版本
+    printf("OpenGL 版本: %s\n", glGetString(GL_VERSION));
+	// main loop
+	while(true){
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		if(event.type == SDL_QUIT){
+			break;
+		}
+		// 设置背景色为深蓝色
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+	}
+
+    // 清理资源
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
